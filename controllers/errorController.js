dcaeecbe25557ece8feea8195c220cde2b71f3dc
Tesla <bolarinwaahmed22@gmail.com>
2022-06-19 21:27:40 +0100

    Installed cross-env to fix node_env issues

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,9 +6,9 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateValueErrorDB = (err) => {
-  const dupValue = Object.keys(err.keyValue)[0];
-  console.log(dupValue);
-  const message = `exists. `;
+  // const dupValue = Object.keys(err.keyValue)[0];
+  // console.log(dupValue);
+  const message = `Field exists.`;
   return new AppError(message, 400);
 };
 
@@ -22,14 +22,14 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  // Operational / Trusted errors
+  // Operational / Trusted  : Send error message to cleint
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
   }
-  // Programming or other unknown errors
+  // Programming or other unknown errors: Send generic error mesage
   else {
     // Log the error to the console
     console.error('ERROR ', err);
@@ -46,12 +46,22 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 is internal server error
   err.status = err.status || 'error'; // error is the status for 500 codes
+
+  // Error in development mode
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  }
+
+  // Error in production mode
+  else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
-    if ((error.name = 'CastError')) error = handleCastErrorDB(error);
-    if ((error.code = 11000)) error = handleDuplicateValueErrorDB(error);
+    // console.log('An error occurred', err);
+    if ((error.code = 11000)) {
+      error = handleDuplicateValueErrorDB(error);
+    }
+    if ((error.name = 'CastError')) {
+      error = handleCastErrorDB(error);
+    }
     sendErrorProd(error, res);
   }
   next();
